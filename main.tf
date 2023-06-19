72746400c4c6ec6a4c4796da8103ee994a897654
Shravang007 <shravangoudgajula@gmail.com>
2023-06-19 21:47:32 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_ssm_parameter" "params" {
+  count = length(var.params)
+  name  = var.params[count.index].name
+  type  = var.params[count.index].type
+  value = var.params[count.index].value
+}
+
+variable "params" {
+  default = [
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopspractice73.online:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopspractice73.online:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopspractice73.online:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspractice73.online:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspractice73.online:8080/", type = "String" },
+    { name = "roboshop.dev.cart.redis_host", value = "redis-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.cart.catalogue_port", value = "8080", type = "String" },
+    { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
+    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://mongodb-dev.devopspractice73.online:27017/catalogue", type = "String" },
+    { name = "roboshop.dev.payment.cart_host", value = "cart-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.payment.cart_port", value = "8080", type = "String" },
+    { name = "roboshop.dev.payment.user_host", value = "user-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.payment.user_port", value = "8080", type = "String" },
+    { name = "roboshop.dev.payment.amqp_host", value = "rabbitmq-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.payment.amqp_user", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.devopspractice73.online:8080", type = "String" },
+    { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
+    { name = "roboshop.dev.user.redis_host", value = "redis-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.devopspractice73.online:27017/users", type = "String" },
+
+
+    #Passwords never keep in Git Repos. It will keep in parameter store manually
+    { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
+
+  ]
+}
