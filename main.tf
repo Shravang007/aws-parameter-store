18c7cc57bc770ed9ec64da0039654716ce798da4
Shravang007 <shravangoudgajula@gmail.com>
2023-06-19 19:49:21 +0530

    terraform

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ variable "params" {
     { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopspractice73.online:8080/", type = "String" },
     { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopspractice73.online:8080/", type = "String" },
     { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspractice73.online:8080/", type = "String" },
-    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspractice73.online:8080/", type = "String" }
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspractice73.online:8080/", type = "String" },
 
 
     }
