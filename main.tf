32abdd301ea98eaa787bc5ef4f970cd166f36934
Shravang007 <shravangoudgajula@gmail.com>
2023-08-11 17:11:15 +0530

    DEV Parameters

--- main.tf
+++ main.tf
@@ -8,49 +8,49 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-#    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopspractice73.online/", type = "String" },
-#    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopspractice73.online/", type = "String" },
-#    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopspractice73.online/", type = "String" },
-#    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspractice73.online/", type = "String" },
-#    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspractice73.online/", type = "String" },
-#
-#    { name = "roboshop.dev.cart.redis_host", value = "elasticache-dev.fjiben.ng.0001.use1.cache.amazonaws.com", type = "String" },
-#    { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.devopspractice73.online", type = "String" },
-#    { name = "roboshop.dev.cart.catalogue_port", value = "80", type = "String" },
-#
-#    { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
-#    { name = "roboshop.dev.catalogue.docdb_endpoint", value = "docdb-dev.cluster-cq66jjycz9yh.us-east-1.docdb.amazonaws.com", type = "String" },
-#
-#
-#    { name = "roboshop.dev.payment.cart_host", value = "cart-dev.devopspractice73.online", type = "String" },
-#    { name = "roboshop.dev.payment.cart_port", value = "80", type = "String" },
-#    { name = "roboshop.dev.payment.user_host", value = "user-dev.devopspractice73.online", type = "String" },
-#    { name = "roboshop.dev.payment.user_port", value = "80", type = "String" },
-#    { name = "roboshop.dev.payment.amqp_host", value = "rabbitmq-dev.devopspractice73.online", type = "String" },
-#    { name = "roboshop.dev.payment.amqp_user", value = "roboshop", type = "String" },
-#
-#    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.devopspractice73.online:80", type = "String" },
-#    { name = "roboshop.dev.shipping.db_host", value = "rds-dev.cluster-cq66jjycz9yh.us-east-1.rds.amazonaws.com", type = "String" },
-#
-#    { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
-#    { name = "roboshop.dev.user.redis_host", value = "elasticache-dev.fjiben.ng.0001.use1.cache.amazonaws.com", type = "String" },
-#
-#    { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
-#    { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
-#    { name = "roboshop.dev.mysql.endpoint", value = "rds-dev.cluster-cq66jjycz9yh.us-east-1.rds.amazonaws.com", type = "String" },
-#
-#    { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
-#    { name = "roboshop.dev.user.docdb_endpoint", value = "docdb-dev.cluster-cq66jjycz9yh.us-east-1.docdb.amazonaws.com", type = "String" },
-#
-#
-#
-#    #Passwords never keep in Git Repos. It will keep in parameter store manually
-#    { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
-#    { name = "roboshop.dev.mysql.password", value = "roboshop123", type = "SecureString" },
-#    { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
-#    { name = "roboshop.dev.docdb.password", value = "roboshop123", type = "SecureString" },
-#    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cq66jjycz9yh.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
-#    { name = "roboshop.dev.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cq66jjycz9yh.us-east-1.docdb.amazonaws.com:27017/user?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopspractice73.online/", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopspractice73.online/", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopspractice73.online/", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspractice73.online/", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspractice73.online/", type = "String" },
+
+    { name = "roboshop.dev.cart.redis_host", value = "elasticache-dev.fjiben.ng.0001.use1.cache.amazonaws.com", type = "String" },
+    { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.cart.catalogue_port", value = "80", type = "String" },
+
+    { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
+    { name = "roboshop.dev.catalogue.docdb_endpoint", value = "docdb-dev.cluster-cq66jjycz9yh.us-east-1.docdb.amazonaws.com", type = "String" },
+
+
+    { name = "roboshop.dev.payment.cart_host", value = "cart-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.payment.cart_port", value = "80", type = "String" },
+    { name = "roboshop.dev.payment.user_host", value = "user-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.payment.user_port", value = "80", type = "String" },
+    { name = "roboshop.dev.payment.amqp_host", value = "rabbitmq-dev.devopspractice73.online", type = "String" },
+    { name = "roboshop.dev.payment.amqp_user", value = "roboshop", type = "String" },
+
+    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.devopspractice73.online:80", type = "String" },
+    { name = "roboshop.dev.shipping.db_host", value = "rds-dev.cluster-cq66jjycz9yh.us-east-1.rds.amazonaws.com", type = "String" },
+
+    { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
+    { name = "roboshop.dev.user.redis_host", value = "elasticache-dev.fjiben.ng.0001.use1.cache.amazonaws.com", type = "String" },
+
+    { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.mysql.endpoint", value = "rds-dev.cluster-cq66jjycz9yh.us-east-1.rds.amazonaws.com", type = "String" },
+
+    { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.user.docdb_endpoint", value = "docdb-dev.cluster-cq66jjycz9yh.us-east-1.docdb.amazonaws.com", type = "String" },
+
+
+
+    #Passwords never keep in Git Repos. It will keep in parameter store manually
+    { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.mysql.password", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.docdb.password", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cq66jjycz9yh.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+    { name = "roboshop.dev.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cq66jjycz9yh.us-east-1.docdb.amazonaws.com:27017/user?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
 
 
     ##Prod ENV
